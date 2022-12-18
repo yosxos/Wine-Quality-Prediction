ae9c8eb42ada894dee5724c9639de031e2a1f22a
yasait <yassirx2012@gmail.com>
2022-12-18 23:43:22 +0100

    Adding methode to save .env

--- domaine/ml_model.py
+++ domaine/ml_model.py
@@ -13,30 +13,6 @@ path="domaine/finalized_model.pkl"
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 #Train the model only if it's the first lunch
-if not os.path.exists(path) :
-    df=pd.read_csv("datasource/Wines.csv")
-    df=df.drop(["Id"],axis=1)
-    X = df.drop(['quality'], axis = 1)
-    y = df['quality']
-    # Normalize feature variables
-    X_features = X
-    X = StandardScaler().fit_transform(X)
-    # Splitting the data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, random_state=0)
-    model = RandomForestClassifier(random_state=1)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    rnd_score = model.score(X_test,y_test)
-    rnd_MSE = mean_squared_error(y_test,y_pred)
-    os.environ["Error"]=str(rnd_MSE)
-    os.environ["Accuracy"]=str(rnd_score)
-    dotenv.set_key("domaine/.env", "Error", os.environ["Error"])
-    dotenv.set_key("domaine/.env", "Accuracy", os.environ["Accuracy"])
-    filename = 'domaine/finalized_model.pkl'
-    pickle.dump(model, open(filename, 'wb'))
-
-
-
 
 def add_data(wine):
     """Add a wine to the data base
@@ -54,20 +30,23 @@ def retrain_model():
     X = df.drop(['quality'], axis = 1)
     y = df['quality']
     # Normalize feature variables
-    X_features = X
     X = StandardScaler().fit_transform(X)
     # Splitting the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, random_state=0)
     model = RandomForestClassifier(random_state=1)
     model.fit(X_train, y_train)
+    rnd_score = model.score(X_test,y_test)
+    rnd_MSE = mean_squared_error(y_test,y_pred)
+    ser_Env(str(rnd_MSE),str(rnd_score))
     y_pred = model.predict(X_test)
-    os.environ["Error"]=str(rnd_MSE)
-    os.environ["Accuracy"]=str(rnd_score)
-    dotenv.set_key("domaine/.env", "Error", os.environ["Error"])
-    dotenv.set_key("domaine/.env", "Accuracy", os.environ["Accuracy"])
     filename = 'domaine/finalized_model.pkl'
     pickle.dump(model, open(filename, 'wb'))
-    
+
+def ser_Env(acc,err):
+    os.environ["Error"]=err
+    os.environ["Accuracy"]=acc
+    dotenv.set_key("domaine/.env", "Error", os.environ["Error"])
+    dotenv.set_key("domaine/.env", "Accuracy", os.environ["Accuracy"])
 def get_model_info():
     """Get current model info
     """
@@ -80,3 +59,21 @@ def get_model_info():
 
     }
 
+if not os.path.exists(path) :
+    df=pd.read_csv("datasource/Wines.csv")
+    df=df.drop(["Id"],axis=1)
+    X = df.drop(['quality'], axis = 1)
+    y = df['quality']
+    # Normalize feature variables
+    X_features = X
+    X = StandardScaler().fit_transform(X)
+    # Splitting the data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, random_state=0)
+    model = RandomForestClassifier(random_state=1)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    rnd_score = model.score(X_test,y_test)
+    rnd_MSE = mean_squared_error(y_test,y_pred)
+    ser_Env(str(rnd_MSE),str(rnd_score))
+    filename = 'domaine/finalized_model.pkl'
+    pickle.dump(model, open(filename, 'wb'))
